ccd64da51dfa7275d91fbfb0631c7aad87a98eba
Francesca Pozzolo <pozzolofrance@gmail.com>
2021-05-18 01:45:06 -0300

    client build one

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require ('mongoose')
 
-mongoose.connect("mongodb+srv://francescapozzolo:greta1165@cluster0.qwgly.mongodb.net/ReViewers?retryWrites=true&w=majority", {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
